5c29b9e0460b0ecd6052952b899d565434495a6b
Yulhee Kim <yulhee@Yulhees-MBP.attlocal.net>
2020-04-09 18:41:22 -0700

    edit admin & database

--- kim.yulhee/admin/users.php
+++ kim.yulhee/admin/users.php
@@ -2,54 +2,130 @@
 
 include "../lib/php/functions.php";
 
-$users = getData("../data/users.json");
+$filename = "../data/users.json";
+$users = getData($filename);
+
+// CRUD = CREATE READ UPDATE DELETE
+
+// file_put_contents, json_encode, explode, $_POST
+
+
+// print_p([$_GET,$_POST]);
+
+$empty_user = (object)[
+	"name"=>"",
+	"type"=>"",
+	"email"=>"",
+	"classes"=>[]
+];
 
 
 
+if(isset($_GET['action'])) {
+	if($_GET['action']=="update") {
+		$users[$_GET['id']]->name = $_POST['user-name'];
+		$users[$_GET['id']]->type = $_POST['user-type'];
+		$users[$_GET['id']]->email = $_POST['user-email'];
+		$users[$_GET['id']]->classes = explode(", ",$_POST['user-classes']);
 
-function showUserPage($id, $user) {
-$classes = implode(", ",$user->classes);
-echo <<<HTML
-<div>
-	<a href="admin/users.php">Back</a>
-</div>
-<div>
-	<h2>$user->name</h2>
-	<div>
-		<strong>Type</strong>
-		<span>$user->type</span>
-	</div>
-	<div>
-		<strong>Email</strong>
-		<span>$user->email</span>
-	</div>
-	<div>
-		<strong>Classes</strong>
-		<span>$classes</span>
-	</div>
-	<div>
-		<a href="admin/users.php?id=$id&edit=true">Edit</a>
-	</div>
-</div>
-HTML;
+		file_put_contents($filename, json_encode($users));
+
+		header("location:{$_SERVER['PHP_SELF']}?id={$_GET['id']}");
+	}
+	if($_GET['action']=="create") {
+		$empty_user->name = $_POST['user-name'];
+		$empty_user->type = $_POST['user-type'];
+		$empty_user->email = $_POST['user-email'];
+		$empty_user->classes = explode(", ",$_POST['user-classes']);
+
+		$id = count($users);
+
+		$users[] = $empty_user;
+		// array_push($users,$empty_user);
+
+		file_put_contents($filename, json_encode($users));
+
+		header("location:{$_SERVER['PHP_SELF']}?id=$id");
+	}
+	if($_GET['action']=="delete") {
+
+		array_splice($users, $_GET['id'], 1);
+
+		file_put_contents($filename, json_encode($users));
+
+		header("location:{$_SERVER['PHP_SELF']}");
+	}
+
 }
 
-function showUserEditPage($id, $user) {
+
+
+
+
+function showUserPage($user) {
+
+$id = $_GET['id'];
 $classes = implode(", ",$user->classes);
-echo <<<HTML
+$addoredit = $id=='new' ? 'Add' : 'Edit';
+$createorupdate = $id=='new' ? 'create' : 'update';
+
+
+
+$userdata = $id=='new' ? "" : <<<HTML
+<div class="card soft">
+<h2>$user->name</h2>
 <div>
-	<a href="admin/users.php?id=$id&edit=false">Back</a>
+	<strong>Type</strong>
+	<span>$user->type</span>
 </div>
 <div>
-	<h2>$user->name</h2>
-	<form action="" class="form form-control" method="post">
-		<label for="type">Type</label>
-		<input class="form-basic" type="text" id="type" name="type" value=$user->type>
-		<label for="email">Email</label>
-		<input class="form-basic" type="text" id="email" name="email" value=$user->email>
-		<label for="classes">Classes</label>
-		<input class="form-basic" type="text" id="classes" name="classes" value=$classes>
-		<input class="form-basic-button" type="submit" name="submit" value="submit">
+	<strong>Email</strong>
+	<span>$user->email</span>
+</div>
+<div>
+	<strong>Classes</strong>
+	<span>$classes</span>
+</div>
+</div>
+HTML;
+
+
+
+echo <<<HTML
+<div class="card soft">
+	<nav class="nav-pills">
+		<ul>
+			<li class="flex-none"><a href="{$_SERVER['PHP_SELF']}">Back</a></li>
+			<li class="flex-stretch"></li>
+			<li class="flex-none"><a href="{$_SERVER['PHP_SELF']}?id=$id&action=delete">Delete</a></li>
+		</ul>
+	</nav>
+</div>
+<div class="grid gap">
+	<div class="col-xs-12 col-md-4">$userdata</div>
+	<form method="post" action="{$_SERVER['PHP_SELF']}?id=$id&action=$createorupdate" class="col-xs-12 col-md-8">
+		<div class="card soft">
+		<h2>$addoredit User</h2>
+		<div class="form-control">
+			<label for="user-name" class="form-label">Name</label>
+			<input type="text" class="form-input" placeholder="A User Name" id="user-name" name="user-name" value="$user->name">
+		</div>
+		<div class="form-control">
+			<label for="user-type" class="form-label">Type</label>
+			<input type="text" class="form-input" placeholder="A User Type" id="user-type" name="user-type" value="$user->type">
+		</div>
+		<div class="form-control">
+			<label for="user-email" class="form-label">Email</label>
+			<input type="text" class="form-input" placeholder="A User Email" id="user-email" name="user-email" value="$user->email">
+		</div>
+		<div class="form-control">
+			<label for="user-classes" class="form-label">Classes</label>
+			<input type="text" class="form-input" placeholder="A User's Classes" id="user-classes" name="user-classes" value="$classes">
+		</div>
+		<div class="form-control">
+			<input type="submit" value="Submit" class="form-button">
+		</div>
+		</div>
 	</form>
 </div>
 HTML;
@@ -64,65 +140,63 @@ HTML;
 
 	<?php include "../parts/meta.php" ?>
 </head>
-
-
 <body>
 
 	<header class="navbar">
 		<div class="container display-flex">
 			<div class="flex-stretch">
 				<img src="img/logo.png" alt="logo" width="50px" style="padding-top: 20px;">
-				<h4 style="display: inline-block; padding-bottom:0px; margin-bottom:0px;">User Admin</h4>
+				<h1>User Admin</h1>
 			</div>
 			<nav class="nav flex-none">
 				<ul class="display-flex">
-					<li><a href="admin/users.php">User List</a></li>
+					<li><a href="<?= $_SERVER['PHP_SELF'] ?>">User List</a></li>
+					<li><a href="<?= $_SERVER['PHP_SELF'] ?>?id=new">Add New User</a></li>
 				</ul>
 			</nav>
 		</div>
 	</header>
 
 	<div class="container">
+
+		<?php
+
+		if(isset($_GET['id'])) {
+
+			// Conditional Ternary
+			// boolean ? iftruedothis : iffalsedothis
+			showUserPage(
+				$_GET['id']=="new" ?
+				$empty_user :
+				$users[$_GET['id']]
+			);
+
+		} else {
+
+
+		?>
 		<div class="card soft">
+		<h2>User List</h2>
 
-			<?php
+		<nav class="nav">
+		<ul>
+		<?php
 
-			if(isset($_GET['edit'])) {
+		foreach($users as $i=>$user) {
+			echo "<li>
+				<a href='admin/users.php?id=$i'>$user->name</a>
+			</li>";
+		}
 
-				if ($_GET['edit'] == 'true') {
-
-					showUserEditPage($_GET['id'], $users[$_GET['id']]);
-
-				} else {
-
-					showUserPage($_GET['id'], $users[$_GET['id']]);
-
-				}
-
-			} else {
-
-			?>
-
-			<h2>User List</h2>
-
-			<ol>
-			<?php
-
-			foreach($users as $i=>$user) {
-				echo "<li>
-					<a href='admin/users.php?id=$i&edit=false'>$user->name</a>
-				</li>";
-			}
-
-			?>
-			</ol>
-
-			<?php
-
-			}
-
-			?>
+		?>
+		</ul>
+		</nav>
 		</div>
+		<?php
+
+		}
+
+		?>
 	</div>
 	
 </body>
---
+++ kim.yulhee/data/test.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 09, 2020 at 07:37 PM
+-- Server version: 5.6.41-84.1
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `yulheell_wnm608`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(13) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `url` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `category` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `date_create` datetime NOT NULL,
+  `date_modify` datetime NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `thumbnail` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `images` varchar(512) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id`, `name`, `email`, `url`, `price`, `category`, `date_create`, `date_modify`, `description`, `thumbnail`, `images`) VALUES
+(1, 'Brown Teddy Bear', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '7.00', 'stuffed animal', '2020-04-09 19:01:17', '2020-04-09 19:01:17', 'This is cute teddy bear. We bought this from IKEA. As you see, condition is really good and clean.', 'images/stuffed_animal_1_thumbnail.jpg', 'images/stuffed_animal_1.jpg'),
+(2, 'Cute White Sheep', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '7.00', 'stuffed animal', '2020-04-09 19:04:15', '2020-04-09 19:04:15', 'This is little stuffed animal from Pottery barn kids. This is super famous for babies.', 'images/stuffed_animal_2_thumbnail.jpg', 'images/stuffed_animal_2.jpg'),
+(3, 'My First Puppy', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '7.00', 'stuffed animal', '2020-04-09 19:26:34', '2020-04-09 19:26:34', 'Condition is good.', 'images/stuffed_animal_3_thumbnail.jpg', 'images/stuffed_animal_3.jpg'),
+(4, 'Gray Small Rabbit', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '7.00', 'stuffed animal', '2020-04-09 19:29:46', '2020-04-09 19:29:46', 'We bought it from IKEA. This is cute rabbit.', 'images/stuffed_animal_4_thumbnail.jpg', 'images/stuffed_animal_4.jpg'),
+(5, 'Kakao Friends Doggie', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '15.00', 'stuffed animal', '2020-04-09 19:30:41', '2020-04-09 19:30:41', 'This doll is from Kakao Friends. Condition is super good. We bought it recently, so it is little expensive compared to other dolls.', 'images/stuffed_animal_5_thumbnail.jpg', 'images/stuffed_animal_5.jpg'),
+(6, 'My First Bear', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '3.00', 'stuffed animal', '2020-04-09 19:31:40', '2020-04-09 19:31:40', 'This is cute teddy bear. This doll has a little something on its face. Otherwise, it is clean,', 'images/stuffed_animal_6_thumbnail.jpg', 'images/stuffed_animal_6.jpg'),
+(7, 'California Bear', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '7.00', 'stuffed animal', '2020-04-09 19:32:26', '2020-04-09 19:32:26', 'This is California bear. Condition is good.', 'images/stuffed_animal_7_thumbnail.jpg', 'images/stuffed_animal_7.jpg'),
+(8, 'Minions', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '7.00', 'stuffed animal', '2020-04-09 19:33:14', '2020-04-09 19:33:14', 'Minions! We are selling in two bundles.', 'images/stuffed_animal_8_thumbnail.jpg', 'images/stuffed_animal_8.jpg'),
+(9, 'Rainbow Puppy', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '13.00', 'stuffed animal', '2020-04-09 19:34:11', '2020-04-09 19:34:11', 'As you see, Rainbow Puppy has a lot of buttons on its body. The doll can sing, talk. Your baby definitely should like it.', 'images/stuffed_animal_9_thumbnail.jpg', 'images/stuffed_animal_9.jpg'),
+(10, 'Mini Mouse', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '7.00', 'stuffed animal', '2020-04-09 19:35:11', '2020-04-09 19:35:11', 'This is real Mini Mouse, We bought it from Disney. Condition is really good. My daughter didn\'t use it.', 'images/stuffed_animal_10_thumbnail.jpg', 'images/stuffed_animal_10.jpg'),
+(11, 'Mario & Friends', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '7.00', 'stuffed animal', '2020-04-09 19:35:53', '2020-04-09 19:35:53', 'Mario & Friends. We are selling in two bundles.', 'images/stuffed_animal_11_thumbnail.jpg', 'images/stuffed_animal_11.jpg'),
+(12, 'Cute Doll', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '7.00', 'stuffed animal', '2020-04-09 19:36:40', '2020-04-09 19:36:40', 'This is cute doll. Look at the red hat. Isn\'t is so cute? ', 'images/stuffed_animal_12_thumbnail.jpg', 'images/stuffed_animal_12.jpg'),
+(13, 'Jelly Cat Bunny', '', 'http://coding.yulheekim.com/aau/wnm608/kim.yulhee/index.php', '15.00', 'stuffed animal', '2020-04-09 19:37:23', '2020-04-09 19:37:23', 'This is super famous doll for parents and babies. Everyone likes it.', 'images/stuffed_animal_13_thumbnail.jpg', 'images/stuffed_animal_13.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
