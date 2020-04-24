a6f839ab00758fe324a1922e1bad46af3925cbf6
Yulhee Kim <yulhee@Yulhees-MBP.attlocal.net>
2020-04-23 18:26:26 -0700

    In Class Work Session11_2

--- kim.yulhee/parts/templates.php
+++ kim.yulhee/parts/templates.php
@@ -11,7 +11,7 @@ return $r.<<<HTML
 			</div>
 			<figcaption class="product-description">
 				<div class="product-price">&dollar;$o->price</div>
-				<div class="product-title">$o->title</div>
+				<div class="product-title">$o->name</div>
 			</figcaption>
 		</figure>
 	</a>
